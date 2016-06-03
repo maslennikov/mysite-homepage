cdd0daa469677b2ecf943cfb1ab2b138c5c78f13
Alexey Maslennikov <alexey@maslennikov.me>
2016-06-03 23:11:00 +0200

    New styling for the homepage

--- assets/js/bundle.js
+++
@@ -1,8 +0,0 @@
----
-# this ensures Jekyll reads the file to be transformed into CSS later
----
-
-{% include_relative jquery-1.11.3.min.js %}
-{% include_relative midnight.jquery.min.js %}
-{% include_relative respond.js %}
-{% include_relative main.js %}
